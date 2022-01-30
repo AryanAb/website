93ba5a989113c6387223295406a1dd12017451af
AryanAb <aryanabed48@gmail.com>
2022-01-30 15:54:54 -0500

    Fixed a bug where there were references to a deleted package

--- components/Experiences.js
+++ components/Experiences.js
@@ -2,24 +2,11 @@ import ExperienceCard from "./ExperienceCard";
 import cardStyles from "../styles/experience.module.css"
 
 import { useState } from "react";
-import { Waypoint } from "react-waypoint";
-import { useSpring, animated } from "react-spring";
 
-const Experiences = ({ experiencesData }) => {
-  const [inView, setInView] = useState(false);
-
-  const cardsAnim = useSpring({
-    from: { y: 7000 },
-    to: { y: inView ? 0 : 7000 }
-  });
-
-  return (
-    <Waypoint onEnter={() => setInView(true)} topOffset={100}>
-      <div className={cardStyles.gridWrapper}>
-        {experiencesData.map(experience => <ExperienceCard image={experience.image} company={experience.company} role={experience.role} duration={experience.duration} />)}
-      </div>
-    </Waypoint>
-  );
-};
+const Experiences = ({ experiencesData }) => (
+  <div className={cardStyles.gridWrapper}>
+    {experiencesData.map(experience => <ExperienceCard image={experience.image} company={experience.company} role={experience.role} duration={experience.duration} />)}
+  </div>
+);
 
 export default Experiences;
