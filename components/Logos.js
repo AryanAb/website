8000866c9685373dbc64cf52cb6a929575a56f05
AryanAb <aryanabed48@gmail.com>
2022-07-10 13:08:17 -0400

    Fixed LinkedIn link, Updated blog post

--- components/Logos.js
+++ components/Logos.js
@@ -8,7 +8,7 @@ const Logos = () => (
     <li> <a href="mailto:aryan.abed@protonmail.com" target='_blank'> <div className={logosStyle.container}> <Image src="/images/protonmail-logo.png" layout="fill" alt="Proton Mail" /> </div> </a> </li>
     <li> <a href="misc/cv_2.pdf" target='_blank'> <div className={logosStyle.container}> <Image src="/images/resume.png" layout="fill" alt="Resume" /> </div> </a> </li>
     <li> <a href="https://devpost.com/AryanAbed?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" target='_blank'> <div className={logosStyle.container}> <Image src="/images/DevpostLogo.jpg" layout="fill" alt="Dev Post" /> </div> </a> </li>
-    <li> <a href="https://www.linkedin.com/in/aryan-abed-esfahani-482927b8/" target='_blank'> <div className={logosStyle.container}> <Image src="/images/LinkedInLogo.webp" layout="fill" alt="LinkedIn" /> </div> </a> </li>
+    <li> <a href="https://www.linkedin.com/in/aryanabed/" target='_blank'> <div className={logosStyle.container}> <Image src="/images/LinkedInLogo.webp" layout="fill" alt="LinkedIn" /> </div> </a> </li>
   </ul>
 );
 
